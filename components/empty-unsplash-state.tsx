6fee8d706ee8145be4fdbc8fda721711df469285
Karl <karl@kejk.tech>
2023-08-01 21:44:34 +0100

    Refactor empty Unsplash state component

--- components/empty-unsplash-state.tsx
+++ components/empty-unsplash-state.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function EmptyUnsplashState() {
   return (
-    <div className="grid h-[95vh] w-full place-content-center">
+    <div className="flex h-[95vh] w-full flex-col justify-start">
       <div className="flex flex-col items-center justify-center space-y-4">
         <svg
           className="h-8 w-8 fill-black dark:fill-white"
@@ -14,10 +14,10 @@ function EmptyUnsplashState() {
         >
           <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
         </svg>
-        <div className="text-center font-sans text-2xl text-neutral-800 dark:text-neutral-200">
+        <div className="text-center font-sans text-lg text-neutral-800 dark:text-neutral-200">
           Use the search bar above to find photos from{" "}
           <a href="https://unsplash.com" target="_blank" rel="noreferrer">
-            <span className="font-semibold text-[#2AAAE2]">Unsplash</span>
+            <span className="font-semibold text-[#2AAAE2] decoration-[#2AAAE2]">Unsplash</span>
           </a>
         </div>
       </div>
