9e68a060c4c4aeacb4c242d57a678b92e1768854
Karl Koch <karl@kejk.tech>
2023-08-02 11:20:38 +0100

    fix: invalid source prop

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.pexels.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 }
