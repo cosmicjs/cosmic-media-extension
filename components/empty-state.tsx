247d19d6e11a83153ee6e53f24155ca7f2b2c4fc
Tony Spiro <tspiro@tonyspiro.com>
2023-08-04 12:42:56 -0700

    add: logos

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 
+import { Icons } from "@/components/icons"
+
 export default function EmptyState() {
   return (
-    <div className="flex h-[95vh] w-full flex-col justify-start">
-      <div className="flex flex-col items-center justify-center space-y-4">
-        <div className="text-center font-sans text-lg text-neutral-800 dark:text-neutral-200">
-          Use the search bar above to find media
-        </div>
+    <div>
+      <div className="mb-8 text-center font-sans text-lg text-neutral-800 dark:text-neutral-200">
+        Use the search bar above to find royalty-free media from popular stock
+        media services.
+      </div>
+      <div className="flex justify-center gap-x-10">
+        <Icons.unsplash className="h-10" />
+        <Icons.pexels className="h-10" />
+        <Icons.pixabay className="h-10" />
       </div>
     </div>
   )
