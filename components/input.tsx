0ff70dfcdb82cddfd897243ce859ed691797a874
Tony Spiro <tspiro@tonyspiro.com>
2023-08-05 15:17:16 -0700

    edit: layout

--- components/input.tsx
+++ components/input.tsx
@@ -5,7 +5,7 @@ import { InputProps } from "@/lib/types"
 function Input({ placeholder, onKeyUp }: InputProps) {
   return (
     <input
-      className="h-10 w-full rounded-lg border-2 border-gray-100 bg-gray-50 p-2 text-neutral-700 selection:bg-gray-200 selection:text-neutral-700 placeholder:text-neutral-500 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-cosmic-blue dark:border-dark-gray-100 dark:bg-dark-gray-50 dark:text-neutral-200 dark:selection:bg-dark-gray-300 selection:dark:text-neutral-300"
+      className="h-12 w-full rounded-lg border-2 border-gray-100 bg-gray-50 p-2 text-neutral-700 selection:bg-gray-200 selection:text-neutral-700 placeholder:text-neutral-500 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-cosmic-blue dark:border-dark-gray-100 dark:bg-dark-gray-50 dark:text-neutral-200 dark:selection:bg-dark-gray-300 selection:dark:text-neutral-300 md:mt-[-7px] md:w-1/2"
       id="search-input"
       placeholder={placeholder}
       onKeyUp={onKeyUp}
