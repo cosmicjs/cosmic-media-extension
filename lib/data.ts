53bd0c0a1ce3d9d3f3ea35616dfa78fcb3d13554
Tony Spiro <tspiro@tonyspiro.com>
2023-08-07 11:54:06 -0700

    get keys from env vars

--- lib/data.ts
+++ lib/data.ts
@@ -1,16 +1,14 @@
 import { createBucketClient } from "@cosmicjs/sdk"
 import { createClient } from "pexels"
 
-export const PEXELS_CLIENT = createClient(
-  "LM0Q3RJVf8hGsD65tfD5p1Dcc9VC1aSzBK0dQkFCVoWuatMxAedlwMW3"
-)
-
 export const UNSPLASH_SEARCH_URL = "https://api.unsplash.com/search/photos"
 export const UNSPLASH_ACCESS_KEY =
-  "fd2c5776f4acd4cd209ea51fec419d09591404ef9e357ef6a5eed195023bcd53"
+  process.env.NEXT_PUBLIC_UNSPLASH_KEY
+  
+export const PEXELS_CLIENT = createClient(process.env.NEXT_PUBLIC_PEXELS_KEY || '')
 
 export const PIXABAY_SEARCH_URL = "https://pixabay.com/api/"
-export const PIXABAY_KEY = "38593288-af492e5b395b816abb4992bcf"
+export const PIXABAY_KEY = process.env.NEXT_PUBLIC_PIXABAY_KEY
 
 export const cosmic = (bucketSlug: string, readKey: string, writeKey: string) =>
   createBucketClient({
