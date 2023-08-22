d3d9173913c8dcb0c421881bca5741543566dab9
Karl Koch <karl@kejk.tech>
2023-08-22 20:22:59 +0100

    fixes to mobile

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -14,15 +14,15 @@ export default function EmptyState() {
         Use the search bar above to find royalty-free media from popular online
         media services.
       </div>
-      <div className="m-auto mb-16 flex max-w-3xl justify-center gap-x-10">
-        <Icons.unsplash className="h-6 sm:h-10" />
-        <Icons.giphy className="h-6 sm:h-10" />
-        <Icons.openai className="h-6 sm:h-10" />
-        <Icons.pexels className="h-6 sm:h-10" />
-        <Icons.pixabay className="h-6 sm:h-10" />
+      <div className="m-auto mb-16 grid w-full grid-cols-2 place-items-center justify-center gap-6 sm:max-w-3xl sm:grid-cols-5 sm:gap-8">
+        <Icons.unsplash className="h-6" />
+        <Icons.giphy className="h-6" />
+        <Icons.openai className="h-6" />
+        <Icons.pexels className="h-6" />
+        <Icons.pixabay className="h-6" />
       </div>
       {location !== "media-modal" && (
-        <div className="flex justify-center gap-8 text-center text-sm">
+        <div className="flex flex-col items-center justify-center gap-8 text-center text-sm sm:flex-row">
           <div>
             <a
               className="flex flex-col sm:flex-row"
