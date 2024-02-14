5f15da545344c11111da1d0c8e9be0cf7fd32186
LUL ✔ <113495911+LULdev@users.noreply.github.com>
2024-02-14 21:47:38 +0100

    Update empty-state.tsx
    xxxxxxxxxxxxxx

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -18,13 +18,38 @@ export default function EmptyState() {
         Use the search bar above to find royalty-free media from popular online
         media services.
       </div>
-     
+      <div className="m-auto mb-16 flex w-full flex-wrap justify-center gap-6 sm:max-w-3xl sm:flex-row sm:flex-nowrap sm:gap-8">
+        <Icons.unsplash className="h-6 sm:h-10" />
+        <Icons.giphy className="h-6 sm:h-10" />
+        <Icons.openai className="h-6 sm:h-10" />
+        <Icons.pexels className="h-6 sm:h-10" />
+        <Icons.pixabay className="h-6 sm:h-10" />
+      </div>
       {location !== "media-modal" && (
         <div className="flex flex-col items-center justify-center gap-8 text-center text-sm sm:flex-row">
           <div>
-            
+            <a
+              className="flex flex-col sm:flex-row"
+              href="https://www.lul.bz"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="mr-2">Made by LUL</div>
-          
+              <div className="mt-2 flex justify-center sm:-mt-1">
+                <Icons.cosmic className="h-6 w-6" />
+              </div>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.lul.bz"
+              target="_blank"
+              rel="noreferrer"
+            >
+              • Made by LUL{" "}
+              <ArrowUpRight className="relative -top-1 inline h-4 w-4" />
+            </a>
+          </div>
         </div>
       )}
     </div>
